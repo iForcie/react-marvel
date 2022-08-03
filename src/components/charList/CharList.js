79aa516a7b324cd4b9cb12cd9158147fcc687c53
iForcie <iForcie>
2022-08-03 12:54:15 +0300

    Work with info about char

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -47,7 +47,8 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
@@ -58,7 +59,7 @@ class CharList extends Component {
             <ul className="char__grid">
                 {items}
             </ul>
-        )
+        );
     }
 
     render() {
@@ -69,7 +70,7 @@ class CharList extends Component {
 
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !(errorMessage || spinner) ? {items} : null;
+        const content = !(errorMessage || spinner) ? items : null;
 
         return (
             <div className="char__list">
